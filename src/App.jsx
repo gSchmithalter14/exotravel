a4d25be3a95eb24218e4414adaa89e7ba672ad4c
Guillermo Schmithalter <gschmithalter14@gmail.com>
2020-11-11 16:28:19 +0100

    added routing setup in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Home from './pages/home';
+import Results from './pages/results';
+import Favorites from './pages/favorites';
+import Favorite from './pages/favorite';
 
 function App() {
   return (
-    <h1>TRAVEL APP</h1>
+    <Router>
+      <nav>Navbar goes here!</nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/results" component={Results} />
+        <Route path="/favorites" exact component={Favorites} />
+        <Route path="/favorites/favorite" component={Favorite} />
+      </Switch>
+    </Router>
   );
 }
 
