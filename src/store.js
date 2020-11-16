8de98350c2a8b9f9f124e5bb2b7601c6ab81870f
Guillermo Schmithalter <gschmithalter14@gmail.com>
2020-11-16 19:08:15 +0100

    Persisting redux store to localStorage

--- src/store.js
+++ src/store.js
@@ -3,12 +3,37 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer';
 
-const initialState = {};
+function saveToLocalStorage(state) {
+  try {
+    const serializedData = JSON.stringify(state);
+    localStorage.setItem('state', serializedData);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
 
 const store = createStore(
   rootReducer,
-  initialState,
+  persistedState,
   composeWithDevTools(applyMiddleware(thunk)),
 );
 
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
 export default store;
+
+// Your list is empty
+// Explore the world now.
