8e8f3e18f1238a0c91fbfb2042019f715ca06bcb
Guillermo Schmithalter <gschmithalter14@gmail.com>
2020-11-17 13:25:01 +0100

    deleted unused files

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Global from './Global.styled';
 import Home from './pages/home';
 import Results from './pages/results';
 import Favorites from './pages/favorites/Favorites';
-import Favorite from './pages/favorite';
 import Navbar from './components/navbar';
 import theme from './theme';
 
@@ -19,7 +18,6 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/results/:keyword" exact component={Results} />
           <Route path="/favorites" exact component={Favorites} />
-          <Route path="/favorites/:id" exact component={Favorite} />
         </Switch>
       </ThemeProvider>
     </Router>
